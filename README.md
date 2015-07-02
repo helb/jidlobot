a99ffcbb7899d00411d593a826d532333683ee7a
Jiří Helebrant <jiri.helebrant@nic.cz>
2015-07-02 16:45:17 +0200

    first version with 3 pubs and poor formatting

---
+++ README.md
@@ -0,0 +1,56 @@
+Plugin for [python-rtmbot](https://github.com/slackhq/python-rtmbot) – get lunch menus sent into channel daily.
+
+## Installation
+
+Create environment:
+
+```
+$ virtualenv -p `which python2` jidlobot
+$ cd jidlobot
+$ source bin/activate
+```
+
+Install rtmbot:
+
+```
+$ git clone git@github.com:slackhq/python-rtmbot.git
+$ cd python-rtmbot
+$ pip install -r requirements.txt
+```
+
+Install plugin:
+
+```
+$ cd plugins
+$ git submodule add git@github.com:helb/jidlobot.git
+$ cd jidlobot
+$ pip install -r requirements.txt
+```
+
+Configure:
+
+```
+$ cd ../..
+$ cp doc/example-config/rtmbot.conf .
+```
+
+Edit `rtmbot.conf` and set `SLACK_TOKEN`, `JIDLOBOT_CHANNEL` and `JIDLOBOT_TIME`.
+
+Channel IDs can be found at `https://slack.com/api/channels.list?token=…`
+
+```
+JIDLOBOT_CHANNEL: "C123488FOO"
+JIDLOBOT_TIME: "10:50"
+```
+
+Run the bot:
+
+```
+$ ./rtmbot.py
+```
+
+
+## TODO
+
+ - support for multiple channels
+ - more pubs
---
+++ jidlobot.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from bs4 import BeautifulSoup
+import urllib2
+import locale
+import re
+
+locale.setlocale(locale.LC_ALL, "cs_CZ.UTF-8")
+
+config_channel = config["JIDLOBOT_CHANNEL"]
+
+config_time = config["JIDLOBOT_TIME"].split(":")
+config_hours = int(time[0])
+config_mins = int(time[1])
+
+crontable = []
+outputs = []
+
+crontable.append([60, "check_time"])
+
+
+def fetch_menu():
+    """
+    Gets today's menu in markdown format.
+    """
+
+    def brana():
+        """
+        U Malické brány
+        """
+
+        menu = ""
+        amounts = []
+        names = []
+        prices = []
+        url = "http://www.plzen-info.cz/umalickebrany/index.php?akce=tydenni_nabidka"
+
+        html = BeautifulSoup(urllib2.urlopen(url).read())
+        for i in html.findAll("table", {"width": "99%"}):
+            x = 0
+            for j in i.findAll("td", {"width": "10%"}):
+                if x % 2 != 0:
+                    prices.append(u"" + j.text.replace(u",-  Kč", u" Kč").strip())
+                else:
+                    amounts.append(j.text.strip())
+                x += 1
+
+            for j in i.findAll("td", {"width": "53%"}):
+                names.append(j.text.strip())
+
+        x = 0
+        while x < len(names):
+            line = u"• " + re.sub(r"^g$", r"", amounts[x]) + " " + names[x] + " " + prices[x]
+            line = re.sub(r"\s{2,}", r" ", line) + "\n"
+            menu += line
+            x += 1
+
+        return u"*U Malické brány:*\n\n" + menu
+
+    def excelent():
+        """
+        Excelent Comix Pub
+        """
+
+        menu = ""
+        url = "https://www.zomato.com/plzen/comix-excelent-urban-pub-plze%C5%88"
+
+        html = BeautifulSoup(urllib2.urlopen(url).read())
+        for i in html.findAll("div", {"class": "tmi-daily"}):
+            line = u"• " + re.sub(r"\s{2,}", r" ", i.text.strip()) + "\n"
+            line = re.sub(r"\s{2,}", r" ", line)
+            menu += line
+
+        return u"*Excelent Comix Pub:*\n\n" + menu
+
+    def vegetka():
+        """
+        Vegetka
+        """
+
+        menu = ""
+        names = []
+        prices = []
+        url = "http://www.menicka.cz/1615-vegetka-zdrava-vyziva.html"
+
+        html = BeautifulSoup(urllib2.urlopen(url).read())
+        day = html.findAll("div", {"class": "menicka"})[0]
+
+        for j in day.findAll("div", {"class": "nabidka_1"}):
+            names.append(j.text.strip())
+
+        for j in day.findAll("div", {"class": "cena"}):
+            prices.append(j.text.strip())
+
+        x = 0
+        while x < len(names):
+            line = u"• " + names[x] + " " + prices[x]
+            menu += line + "\n"
+            x += 1
+
+        return u"*Vegetka:*\n\n" + menu
+
+    return brana() + "\n\n" + excelent() + "\n\n" + vegetka() + "\n\n"
+
+
+def send_menu():
+    """
+    Prints today's menu into channel.
+    """
+
+    date = datetime.strftime(datetime.now(), u"%A %-d.%-m.".encode("utf-8")).decode("utf-8").lower()
+    header = u"*Obědy – " + date + ":*\n\n"
+    menu = header + fetch_menu()
+    outputs.append([config_channel, menu])
+
+
+def check_time():
+    """
+    Checks current time and runs according function(s):
+        - 10:35 - 10:50 - send today's menu into channel
+    """
+
+    hour = datetime.now().hour
+    minute = datetime.now().minute
+
+    if hour == config_hours and minute == config_mins:
+        send_menu()
