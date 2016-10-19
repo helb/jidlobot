c911f5af1d8150b0bff81fec0a47955cdaf54521
Jiří Helebrant <jiri.helebrant@nic.cz>
2016-10-19 15:08:18 +0200

    rewrite to python3, use Markdown output for Mattermost compatibility

--- README.md
+++ README.md
@@ -1,63 +1,49 @@
-Plugin for [python-rtmbot](https://github.com/slackhq/python-rtmbot) – get lunch menus sent into channel.
+Mattermost "bot" – get lunch menus sent into channel.
 
 ## Installation
 
-Create environment:
+Clone the repo and install dependencies:
 
 ```
-$ virtualenv -p `which python2` jidlobot
-$ cd jidlobot
-$ source bin/activate
-```
-
-Install rtmbot:
-
-```
-$ git clone https://github.com/helb/python-rtmbot.git
-$ cd python-rtmbot
-$ pip install -r requirements.txt
-```
-
-Install plugin:
-
-```
-$ cd plugins
 $ git clone https://github.com/helb/jidlobot.git
 $ cd jidlobot
+```
+
+Create virtual environment:
+
+```
+$ virtualenv -p `which python3` venv
+$ source venv/bin/activate
+```
+
+Install dependencies:
+
+```
 $ pip install -r requirements.txt
 ```
+
 
 Edit `jidlobot.conf` and set:
 
- - `CHANNEL` to your channel ID – these can be found at `https://slack.com/api/channels.list?token=…`.
+ - `MATTERMOST_KEY` – [Mattermost docs: Incoming Webhooks](https://docs.mattermost.com/developer/webhooks-incoming.html)
  - `HTTP_TIMEOUT` to the number of seconds you would want to wait for each URL
  - `URLS` – list of urls at menicka.cz
- 
-Configure rtmbot:
-
-```
-$ cd ../..
-$ cp doc/example-config/rtmbot.conf .
-```
-
-Edit `rtmbot.conf` and set `SLACK_TOKEN`.
 
 Run the bot:
 
 ```
-$ ./rtmbot.py
+$ ./jidlobot.py
 ```
 
 Create a cron job if everything works.
 
 Example script for running from cron:
 
-```
+```bash
 #!/bin/bash
-cd /path/to/virtualenv
-source bin/activate
-cd python-rtmbot
-./rtmbot.py
+cd /path/to/jidlobot
+source venv/bin/activate
+./jidlobot.py
 ```
 
 Example crontab entry (runs Monday to Friday at 10:58):
@@ -65,12 +51,3 @@ Example crontab entry (runs Monday to Friday at 10:58):
 ```
 58 10 * * 1-5 /path/to/jidlobot.sh
 ```
-
-(Slack sends out e–mail notifications at :00/:15/:30/:45 mins, so it's handy to have the script run a minute or two before that)
-
-*Note: rtmbot has no utf8 support, but it's easy to enable it: https://github.com/slackhq/python-rtmbot/pull/9/*
-
-
-## TODO
-
- - support for multiple channels
--- jidlobot.py
+++ jidlobot.py
@@ -2,8 +2,8 @@
 
 from datetime import datetime
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import yaml
-import urllib2
 import locale
 import re
 import socket
@@ -12,7 +12,7 @@ import sys
 locale.setlocale(locale.LC_ALL, "cs_CZ.UTF-8")
 parser = "html5lib"
 
-with open("plugins/jidlobot/jidlobot.conf", 'r') as conf_file:
+with open("jidlobot.conf", 'r') as conf_file:
     config = yaml.load(conf_file)
 
 
@@ -26,7 +26,7 @@ def fetch_menu(url):
     prices = []
 
     try:
-        html = BeautifulSoup(urllib2.urlopen(url, timeout=config["HTTP_TIMEOUT"]).read(), parser)
+        html = BeautifulSoup(urlopen(url, timeout=config["HTTP_TIMEOUT"]).read(), parser)
         day = html.findAll("div", {"class": "menicka"})[0]
         restaurant = html.findAll("span", {"class": "org"})[0].text
 
@@ -35,12 +35,12 @@ def fetch_menu(url):
             prices.append(j.next_sibling.next_sibling.text.strip())
 
         for i in range(len(names)):
-            line = u"• " + names[i] + " " + prices[i]
+            line = "- " + names[i] + " " + prices[i]
             menu += line + "\n"
 
-        return u"*" + restaurant + ":*\n" + menu
-    except socket.timeout, e:
-        return u"" + url + ": timeout :angry:\n"
+        return "***" + restaurant + ":***\n\n" + menu
+    except socket.timeout:
+        return "" + url + ": timeout :angry:\n"
 
 
 def fetch_lokal_menu(url):
@@ -53,7 +53,7 @@ def fetch_lokal_menu(url):
     prices = []
 
     try:
-        html = BeautifulSoup(urllib2.urlopen(url, timeout=config["HTTP_TIMEOUT"]).read(), parser)
+        html = BeautifulSoup(urlopen(url, timeout=config["HTTP_TIMEOUT"]).read(), parser)
         meals = html.findAll("img", {"alt": config["LOKAL_NAME"]})[0].parent.parent.findAll("div", {"class": "list"})[0].findAll("table")[0].findAll("tr", {"class": None})
 
         for line in meals:
@@ -61,12 +61,12 @@ def fetch_lokal_menu(url):
             prices.append(line.findAll("td")[1].text.strip())
 
         for i in range(len(names)):
-            line = u"• " + names[i] + " " + prices[i]
+            line = "- " + names[i] + " " + prices[i]
             menu += line + "\n"
 
-        return u"*" + config["LOKAL_NAME"] + ":*\n" + menu
-    except socket.timeout, e:
-        return u"" + url + ": timeout :angry:\n"
+        return "***" + config["LOKAL_NAME"] + ":***\n\n" + menu
+    except socket.timeout:
+        return "" + url + ": timeout :angry:\n"
 
 menus = []
 
@@ -75,10 +75,8 @@ for url in config["URLS"]:
 
 menus.append(fetch_lokal_menu(config["LOKAL_URL"]))
 
-date = datetime.strftime(datetime.now(), u"%A %-d.%-m.".encode("utf-8")).decode("utf-8").lower()
-header = u"<!channel> *Obědy – " + date + ":*\n\n"
+date = datetime.strftime(datetime.now(), "%A %-d.%-m.")
+header = "@channel *Obědy – " + date + ":*\n\n"
 message = header + "\n".join(menus)
 
-outputs = []
-outputs.append([config["CHANNEL"], message])
-sys.exit(0)
+print(message)
