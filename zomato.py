6682bf9ee37057f005051e17a87cd78e3062d3b0
Vojtech Myslivec <vojtech@xmyslivec.cz>
2019-03-06 11:40:43 +0100

    zomato: Use cs variant of daily-menu URL

--- zomato.py
+++ zomato.py
@@ -10,7 +10,7 @@ def fetch(url, config):
     prices = []
 
     try:
-        url = re.sub(r"\?.*/?(daily-menu)?$", "/daily-menu", url)
+        url = re.sub(r"\?.*/?(daily-menu|denní-menu)?$", "/denní-menu", url)
         result = requests.get(url, timeout=config["HTTP_TIMEOUT"], headers={
             "User-Agent": config["ZOMATO_UA"],
             "Accept-Language": config["ZOMATO_LANG"],
