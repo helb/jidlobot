e7677a4ee5da0ed25bcba20c413f3faa765b7a63
Vojtech Myslivec <vojtech@xmyslivec.cz>
2020-10-12 11:09:27 +0200

    mattermost: Use @all to mark all members in a channel

--- mattermost.py
+++ mattermost.py
@@ -3,7 +3,7 @@ import json
 
 
 def send_mattermost(body, subject, config):
-    message = "@channel\n\n### " + subject + "\n\n" + body.replace(".", "\.")
+    message = "@all\n\n### " + subject + "\n\n" + body.replace(".", "\.")
     payload = json.dumps({
         "channel": config["MATTERMOST_CHANNEL"],
         "username": config["MATTERMOST_USERNAME"],
